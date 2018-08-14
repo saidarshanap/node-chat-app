6402f31e37340760aa1826c1eddf00b792c696a9
Sai Darshan <darshansai81@gmail.com>
2018-08-14 11:30:05 +0530

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,16 @@ var socket = io();
         jQuery('#messages').append(li);
     });
 
+    socket.on('newLocationMessage',function(message){
+      var li= jQuery('<li></li>');
+      var a= jQuery('<a target="_blank">My Current Location</a>');
+
+      li.text(`${message.from}`);
+      a.attr('href',message.url);
+      li.append(a);
+      jQuery('#messages').append(li);
+    });
+
     jQuery('#message-form').on('submit', function (e){
       e.preventDefault();
 
@@ -25,4 +35,20 @@ var socket = io();
       },function(){
 
       });
+
+      var locationButton = jQuery('#send-location');
+      locationButton.on('Click',function(){
+        if(!navigator.geolocation){
+          return alert('Geolocation Not Supported');
+        }
+        navigator.geolocation.getCurrentPosition(function(position){
+            socket.emit('createLoacationMessage',{
+              latitude : position.coords.latitude,
+              longitude: position.coords.longitude
+            });
+        },function(){
+          alert('Unable to fetch loacation');
+        });
+      });
+
     });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,26 @@
 var expect = require('expect');
 
-var{generateMessage}= require('./message');
+var{generateMessage,generateLocationMessage}= require('./message');
 
 describe('generateMessage',()=>{
-    it('Should generate coreect message object' , ()=>{
+    it('Should generate correct message object' , ()=>{
         var from = 'Dart';
         var text= 'Message';
         var message= generateMessage(from,text);
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from,text});
+    });
+});
+
+describe('generateMessage',()=>{
+    it('Should generate correct location object' , ()=>{
+        var from = 'Dart';
+        var latitude= 15;
+        var longitude= 19;
+        var url= 'https://www.google.com/maps?q=15,19';
+        var message= generateLocationMessage(from,latitude,longitude);
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from,url});
     });
 });
